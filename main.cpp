6f883ea5bc91f2862eca244c80fece9619ad4e8a
keco-mentario <ldfernandezqu@hotmail.com>
2024-10-16 22:29:00 -0600

    Version Final
    Solo se limpio el main para entregar la tarea. Yo la subi con la documentacion y el UML

--- main.cpp
+++ main.cpp
@@ -6,44 +6,9 @@
 #include "Usuario.h"
 
 int main () {
-
- ControladoraMenu* sistema = new ControladoraMenu();
- sistema->control0();
-
- delete sistema;
-    /*string usuariosChat = "usuarios.txt";
-    string historiaChat = "Historial.txt";
-
-    ChatMediador mediador;
-
-    Usuario usuario1 (&mediador, "Alicia");
-    Usuario usuario2 (&mediador, "Cheshire cat");
-    Usuario usuario3 (&mediador, "Sombrerero loco");
-
-
-   mediador.registrar(&usuario1);
-   mediador.registrar(&usuario2);
-   mediador.registrar(&usuario3);
-    cout << "usuarios conectados; "<<endl;
-   cout << mediador.mostrarConectados()<<endl;
-
-    mediador.guardaParticipantes( usuariosChat, &mediador);
-    mediador.cargaParticipantes( usuariosChat, &mediador);
-
-    cout << mediador.mostrarConectados()<<endl;
-
-    usuario1.enviar ("Hola a todos");
-    usuario2.enviar ("Hola Alicia");
-    usuario3.enviar ("Todos estamos locos!");
-    cout <<endl;
-    mediador.enviarMensajePrivado ("hola",&usuario2, &usuario3);
-    cout <<endl;
-    mediador.enviarMensajePrivado ("que tal?",&usuario3, &usuario2);
-
-    mediador.guardaChats(historiaChat, &mediador);
-    mediador.cargaChats(historiaChat, &mediador);
-
-     cout <<mediador.mostrarHistorialChat();*/
+    ControladoraMenu* sistema = new ControladoraMenu();
+    sistema->control0();
+    delete sistema;
 
     return 0;
 }
